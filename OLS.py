4232c0c669a84c9d9d5594d156248c7fecc23fcf
Anthony Li <qiaoyuanlee@hotmail.com>
2022-10-11 01:36:38 -0700

    Add files via upload

--- OLS.py
+++ OLS.py
@@ -30,3 +30,50 @@ from statsmodels.api import OLS
 X_train_subset_transformed_with_const = sm.add_constant(X_train_subset_transformed)
 
 OLS(y_train.reset_index(drop=True),X_train_subset_transformed_with_const).fit().summary()
+
+
+###Evaluate model performance
+
+from sklearn.metrics import mean_squared_error, r2_score,  mean_absolute_error
+def report_regression_metrics(model, X, y, plots=False, y_pred = []):
+    if not len(y_pred):
+        y_pred = model.predict(X)
+    if plots:
+        
+        #plt.figure()
+        #sns.distplot(y - y_pred) 
+        #plt.figure()
+        #sns.scatterplot(x=y, y=pd.DataFrame(y_pred,index=X.index))
+        plt.plot(y, y_pred)
+        
+    print("MSE:", mean_squared_error(y, y_pred))
+    print("R2 score:", r2_score(y, y_pred))
+   # print("Max error:", max_error(y, y_pred))
+    print("mean_absolute_error:", mean_absolute_error(y, y_pred))
+
+print("Train metrics:")
+report_regression_metrics(lr, X_train_subset_transformed, y_train)
+
+print("Val metrics:")
+report_regression_metrics(lr, X_val_subset, y_val)
+
+print("Test metrics:")
+report_regression_metrics(lr, X_test_subset, y_test)
+
+###Print MSE, R^2, mean absolute error on training, validation and test datasets
+
+y_pred = lr.predict(X_train_subset_transformed)
+y_pred = pd.DataFrame(y_pred,index = X_train_subset.index)
+y_pred.head()
+
+plt.plot(y_pred, color = "green")
+plt.plot(y_train, color = "blue")
+plt.title("Predict vs Actual (Training set)")
+plt.xlabel("Quarter-Year")
+plt.ylabel("LG")
+plt.xticks(rotation = 90)
+fig = plt.gcf()
+fig.set_size_inches(24, 8)
+plt.show()
+
+###Visualize the predicted value vs actual value
